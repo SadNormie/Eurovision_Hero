014fd26770e1d6db7ba6f1f5bd52b07a45447db6
SadNormie <69667860+SadNormie@users.noreply.github.com>
2023-03-06 08:31:35 +0100

    Added executable and bug fixes

--- esc_hero.py
+++ esc_hero.py
@@ -5,7 +5,7 @@ def finalists(): #A function for making finalists
     grand_final = []
     p = 0
     print('''Hello, and welcome to Eurovision Hero :)
-    please write every participating country.
+    please write your participating countries of desire in order of who should have the highest odds of winning.
     Should you wish to delete the last country you wrote, write "delete" 
     When you are done, press 'enter' on your keyboard
     ''') #Welcome message
@@ -24,7 +24,7 @@ def finalists(): #A function for making finalists
             grand_final.pop()
             print("\nYour list:")
             for x in range(len(grand_final)):
-                print(grand_final[x])
+                print(x + 1, grand_final[x])
             p -= 1
             
 
@@ -35,7 +35,7 @@ def finalists(): #A function for making finalists
             grand_final.append(add_country) 
             print("\nYour list:")
             for x in range(len(grand_final)):
-                print(grand_final[x])
+                print(x + 1, grand_final[x])
             p += 1 
     return grand_final
 def jury(): #A function for making the jury and the televote
@@ -46,6 +46,7 @@ def jury(): #A function for making the jury and the televote
         same_as_grand_final = input()
         if same_as_grand_final == 'no':
             the_jury = participants.copy()
+            print("")
             jury_loop = False
         elif same_as_grand_final == 'yes':
             while True:
@@ -80,17 +81,67 @@ def brain(): #The function running Eurovision_Hero
     participants = finalists()
     print('Do you want to make your own jury?\n\nyes\nno\n')
     the_jury, the_televote = jury()
-    for y in range(len(participants)):
-        odds = [63, 42, 19, 10, 8, 7, 6, 5, 4, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1]
+    random.shuffle(the_jury), random.shuffle(the_televote)
+    for y in range(len(the_jury)):
+        odds = [63+random.randrange(-61, 32), 
+                42+random.randrange(-40, 21), 
+                19+random.randrange(-17, 10), 
+                10+random.randrange(-8, 5), 
+                8+random.randrange(-6, 4), 
+                7+random.randrange(-5, 4), 
+                6+random.randrange(-4, 3), 
+                5+random.randrange(-3, 3), 
+                4+random.randrange(-2, 2), 
+                3+random.randrange(-1, 2), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                1+random.randrange(0, 1)]
         points = [1, 2, 3, 4, 5, 6, 7, 8, 10, 12]
         odds_by_participant = [[participant, odds[index]] for index, participant in enumerate(participants)]
         winners = []
         value = 0 
+        jury_currently_voting = the_jury[y]
+        if "Greece" in participants and "Cyprus" == jury_currently_voting:
+            for w in range(len(participants)):
+                if "Greece" in odds_by_participant[w][0]:
+                    odds_by_participant[w][1] = 10000
+                    break
+        elif "Cyprus" in participants and "Greece" == jury_currently_voting:
+            for w in range(len(participants)):
+                if "Cyprus" in odds_by_participant[w][0]:
+                    odds_by_participant[w][1] = 10000
+                    break
+        elif "Armenia" in participants and "Azerbaijan" == jury_currently_voting:
+            for w in range(len(participants)):
+                if "Armenia" in odds_by_participant[w][0] and len(participants) > 11:
+                    odds_by_participant[w][0] = jury_currently_voting
+                    break
+        elif "Azerbaijan" in participants and "Armenia" == jury_currently_voting:
+            for w in range(len(participants)):
+                if "Azerbaijan" in odds_by_participant[w][0] and len(participants) > 11:
+                    odds_by_participant[w][0] = jury_currently_voting
+                    break
+
+            
         for x in range(len(odds_by_participant)):
             odds_by_participant[x].append(value)
             value += odds_by_participant[x][1]
             odds_by_participant[x].append(value)
-        jury_currently_voting = the_jury[y]
+        
 
         stopper = len(odds_by_participant)
         if len(odds_by_participant) >= 11:
@@ -103,21 +154,99 @@ def brain(): #The function running Eurovision_Hero
                 
 
         winners.reverse()
+        print("Now calling", jury_currently_voting + "!\n")
         for p in range(len(winners)):
             if p == 0:
                 time.sleep(0.25)
                 print(points[p], "point goes to", winners[p])
             elif p == 9:
                 input("")
-                print(winners[p], "gets ", points[p], "points from ", jury_currently_voting + "!")
+                print(winners[p], "gets", points[p], "points from", jury_currently_voting + "!")
             else:
                 time.sleep(0.25)
                 print(points[p], "points goes to", winners[p])
         input("")
+    print("The Jury has given their votes!\n\n Now it's time to see what viewers has voted:")
+    input("")
+    random.shuffle(participants)
+    the_televote.append("Rest of the world")
 
-            
+    for y in range(len(the_televote)):
+        odds = [63+random.randrange(-61, 32), 
+                42+random.randrange(-40, 21), 
+                19+random.randrange(-17, 10), 
+                10+random.randrange(-8, 5), 
+                8+random.randrange(-6, 4), 
+                7+random.randrange(-5, 4), 
+                6+random.randrange(-4, 3), 
+                5+random.randrange(-3, 3), 
+                4+random.randrange(-2, 2), 
+                3+random.randrange(-1, 2), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                2+random.randrange(0, 1), 
+                1+random.randrange(0, 1)]
+        points = [1, 2, 3, 4, 5, 6, 7, 8, 10, 12]
+        odds_by_participant = [[participant, odds[index]] for index, participant in enumerate(participants)]
+        winners = []
+        value = 0 
+        televote_currently_voting = the_televote[y]
+        if "Armenia" in participants and "Azerbaijan" == televote_currently_voting:
+            for w in range(len(participants)):
+                if "Armenia" in odds_by_participant[w][0] and len(participants) > 11:
+                    odds_by_participant[w][0] = televote_currently_voting
+                    break
+        elif "Azerbaijan" in participants and "Armenia" == televote_currently_voting:
+            for w in range(len(participants)):
+                if "Azerbaijan" in odds_by_participant[w][0] and len(participants) > 11:
+                    odds_by_participant[w][0] = televote_currently_voting
+                    break
         
-            
+        for x in range(len(odds_by_participant)):
+            odds_by_participant[x].append(value)
+            value += odds_by_participant[x][1]
+            odds_by_participant[x].append(value)
+        
+
+        stopper = len(odds_by_participant) - 1
+        if televote_currently_voting == "Rest of the world" and len(odds_by_participant) < 11:
+            stopper = len(odds_by_participant)
+        elif len(odds_by_participant) >= 11:
+            stopper = 11 - 1
+        while len(winners) < stopper:
+            casting_vote = random.randrange(0, value)
+            for x in range(len(odds_by_participant)):
+                if casting_vote > odds_by_participant[x][2] and casting_vote < odds_by_participant[x][3] and odds_by_participant[x][0] not in winners and odds_by_participant[x][0] not in televote_currently_voting:
+                    winners.append(odds_by_participant[x][0])
+        
+
+        winners.reverse()
+        print("Now calling", televote_currently_voting + "!\n")
+        for p in range(len(winners)):
+            if p == 0:
+                time.sleep(0.25)
+                print(points[p], "point goes to", winners[p])
+            elif p == 9:
+                input("")
+                print(winners[p], "gets", points[p], "points from", televote_currently_voting + "!")
+            else:
+                time.sleep(0.25)
+                print(points[p], "points goes to", winners[p])
+        input("")
+    print("You did it! Now add your scores to scorewiz.com and make your scoreboard :)")
 
 brain()
 
